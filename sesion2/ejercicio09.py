ec59eccccca2761177f7adb4271b9dc61e9b590e
MiixZ <xwarzmiixz@gmail.com>
2023-10-03 17:04:38 +0200

    Fixed ejercicio 9

--- sesion2/ejercicio09.py
+++ sesion2/ejercicio09.py
@@ -5,11 +5,13 @@ calcule el número mínimo de paneles solares que se necesitan para producir, al
 en un mes (30 días) teniendo en cuenta que los paneles solares tienen un 17% de rendimiento y que
 son de un tamaño de 1.6 m2."""
 
+import math
+
 print("Introduzca la radiación solar media por día en Kwh/m2:")
 radiacion_solar = float(input())
 
 radiacion_solar_mes = radiacion_solar * 30
-radiacion_solar_mes_rendimiento = radiacion_solar_mes * 0.17 / 1.6
+radiacion_solar_mes_rendimiento = radiacion_solar_mes * 0.17 * 1.6
 paneles_solares = 1000 / radiacion_solar_mes_rendimiento
 
-print("El número mínimo de paneles solares es: ", int(paneles_solares))
+print("El número mínimo de paneles solares es: ", math.ceil(paneles_solares))
