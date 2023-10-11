1b886240a35aabd1952070768c7a69489fdf126a
MiixZ <xwarzmiixz@gmail.com>
2023-10-11 11:48:32 +0200

    Remodelación ejercicio 5 sesión 2

--- sesion2/s2ejercicio05.py
+++ sesion2/s2ejercicio05.py
@@ -3,6 +3,7 @@
 """Hacer un programa para calcular la diferencia en horas:minutos:segundos entre dos instantes de
 tiempo dados en horas:minutos:segundos."""
 
+"""Separo la cadena introducida en una lista con tres elementos: hora, minuto y segundo"""
 print("Introduzca la primera hora con el siguiente formato (hh:mm:ss):")
 hora1 = input()
 hora_minuto_segundo1 = hora1.split(":")
@@ -11,8 +12,10 @@ print("Introduzca la segunda hora con el siguiente formato (hh:mm:ss):")
 hora2 = input()
 hora_minuto_segundo2 = hora2.split(":")
 
+"""Calculo la diferencia entre las horas, minutos y segundos"""
 dif_horas = int(hora_minuto_segundo2[0]) - int(hora_minuto_segundo1[0])
 dif_minutos = int(hora_minuto_segundo2[1]) - int(hora_minuto_segundo1[1])
 dif_segundos = int(hora_minuto_segundo2[2]) - int(hora_minuto_segundo1[2])
 
-print("La diferencia entre las dos horas es: ", abs(dif_horas), ":", abs(dif_minutos), ":", abs(dif_segundos))
+print("La diferencia entre las dos horas es: ", abs(dif_horas), ":",
+      abs(dif_minutos), ":", abs(dif_segundos))
