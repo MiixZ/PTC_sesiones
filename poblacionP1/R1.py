ce28fc7381b7e4ee2611d1fa5e956f586c326f82
MiixZ <xwarzmiixz@gmail.com>
2023-11-07 19:18:12 +0100

    Práctica 1 Inicio R1

---
+++ poblacionP1/R1.py
@@ -0,0 +1,8 @@
+"""
+Calcular la variación de la población por provincias desde el año 2011 a 2017 en términos absolutos y relativos
+generando la página web 1 (variacionprovincias.html) que contenga una tabla como la del pdf.
+Las fórmulas a aplicar:
+Variación absoluta = Población 2017 – Población 2016
+Variación relativa = (Población 2017 – Población 2016) * 100
+                        ----------------------------------------
+"""
