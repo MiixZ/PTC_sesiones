ac9d93d678644592d1691190401d0bc2a962f09d
MiixZ <xwarzmiixz@gmail.com>
2023-11-08 12:03:35 +0100

    Clase 08/11/2023 Comentarios a ejercicio de ejemplo.

--- excepciones/validarExcepciones.py
+++ excepciones/validarExcepciones.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-
+ESTO ES UN EJEMPLO DE VALIDACIÓN DE ENTRADAS USANDO EXCEPCIONES
 @author: Eugenio
 Validación de entradas usando gestión de excepciones
 Contamos las entradas correctas e incorrectas para informar al finalizar
@@ -11,48 +11,37 @@ Solo se aceptan enteros positivos y además que sean múltiplos de 3 y 7 al mism
 
 # vemos si el entero positivo es múltiplo de 3 y 7
 def validarMultiplo3y7(enteroPos):
-    
-    if enteroPos%3==0 and enteroPos%7==0:
-        validacion=True
+    if enteroPos % 3 == 0 and enteroPos % 7 == 0:
+        validacion = True
     else:
-        validacion=False
-    
+        validacion = False
+
     return validacion
 
-    
-    
+
 ##Main    
 
 
+entradasCorrec_a_leer = 4  # pedir 4 valores correctos. Funcionará hasta que lea 4 valores correctos.
+entradasCorrectas = 0
+entradasIncorrectas = 0
+nombreEstudiante = "Manolo García"
 
-entradasCorrec_a_leer=4 # pedir 4 valores correctos
-entradasCorrectas=0
-entradasIncorrectas=0
-nombreEstudiante="Manolo García"
-
-while entradasCorrectas< entradasCorrec_a_leer:    
+while entradasCorrectas < entradasCorrec_a_leer:
     try:
-        entrada=input("Dime un entero positivo múltiplo de 3 y 7: ")
-        valor=int(entrada)
-        assert valor>0, f"La entrada debe ser mayor a cero y ha indicado {valor}"
+        entrada = input("Dime un entero positivo múltiplo de 3 y 7: ")
+        valor = int(entrada)
+        assert valor > 0, f"La entrada debe ser mayor a cero y ha indicado {valor}"
         assert validarMultiplo3y7(valor), f"Debe ser múltiplo de 3 y 7 y {valor} no lo es"
         print(f"La entrada es {valor} y es correcta")
-        entradasCorrectas+=1
+        entradasCorrectas += 1
     except ValueError:
-        print (f"Debe introducir un número entero positivo y su entrada fue: {entrada}")
-        entradasIncorrectas+=1
-    except AssertionError as error:
+        print(f"Debe introducir un número entero positivo y su entrada fue: {entrada}")
+        entradasIncorrectas += 1
+    except AssertionError as error:  # El error es la cadena de caracteres en el assert.
         print(error)
-        entradasIncorrectas+=1
- 
-       
-   
+        entradasIncorrectas += 1
+
 print("Fin del programa de ejemplo de validación con excepciones")
-print("Nombre estudiante: ",nombreEstudiante)
-print("Entradas correctas: ",entradasCorrectas," incorrectas: ",entradasIncorrectas)
-
-
-
-  
-
-
+print("Nombre estudiante: ", nombreEstudiante)
+print("Entradas correctas: ", entradasCorrectas, " incorrectas: ", entradasIncorrectas)
