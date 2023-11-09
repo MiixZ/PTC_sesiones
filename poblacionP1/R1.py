7223fda0f609b795358c7869f1b547c55bed8c11
MiixZ <xwarzmiixz@gmail.com>
2023-11-09 17:37:15 +0100

    Práctica 1 Change: Inicio R1.

--- poblacionP1/R1.py
+++ poblacionP1/R1.py
@@ -11,7 +11,31 @@ import csv
 import bs4
 import numpy as np
 import matplotlib as mpl
+import libreria_comun as lc
+
+
 # Hay que usar diccionarios y numpy arrays. Prohibido
 # usar pandas y dataframe.
 
+
 # Funciones
+def salida_html_R1(fichero):
+    """
+    Genera la página web 1 (variacionprovincias.html)
+    leyendo los datos del fichero que le pasamos como parámetro.
+    Para cada provincia (fila) leerá el total nacional de cada año (columna) y
+    generará una tabla con dos grandes columnas (Variación absoluta y Variación relativa)
+    de cada provincia en el csv.
+    :param fichero: CSV a leer.
+    :return: None
+    """
+
+
+def variacion_relativa(anio_mayor, anio_menor):
+    """
+    Calcula la variación relativa entre dos años.
+    :param anio_mayor: Año mayor.
+    :param anio_menor: Año menor.
+    :return: Variación relativa entre ambos años.
+    """
+    return ((anio_mayor - anio_menor) / anio_menor) * 100
--- poblacionP1/libreria_comun.py
+++ poblacionP1/libreria_comun.py
@@ -1,5 +1,8 @@
 # Funciones globales para R1, R2, R3, R4 y R5.
 
+import csv
+
+
 def round2(numero):
     """
     Redondea un número a dos decimales.
@@ -8,3 +11,13 @@ def round2(numero):
     """
     return round(numero, 2)
 
+
+def leer_anios_csv(fichero):
+    """
+    Lee los años de un fichero CSV.
+    :param fichero: Fichero CSV a leer.
+    :return: Número de años.
+    """
+    with open(fichero, newline='') as f:
+        reader = csv.reader(f)
+        return len(next(reader)) - 1
