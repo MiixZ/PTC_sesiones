76d7549c3bc1773034e9831d347d23cbc08b2ae9
MiixZ <xwarzmiixz@gmail.com>
2023-11-09 16:48:16 +0100

    Práctica 1 ADD: Fichero para una librería común.

---
+++ poblacionP1/libreria_comun.py
@@ -0,0 +1,10 @@
+# Funciones globales para R1, R2, R3, R4 y R5.
+
+def round2(numero):
+    """
+    Redondea un número a dos decimales.
+    :param numero:
+    :return:
+    """
+    return round(numero, 2)
+
