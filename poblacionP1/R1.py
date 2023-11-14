0da8e3ccea63882d83ec2fd9fac0f16465714848
MiixZ <xwarzmiixz@gmail.com>
2023-11-14 12:43:34 +0100

    Práctica 1, R1 ADD: Modularizado código.

--- poblacionP1/R1.py
+++ poblacionP1/R1.py
@@ -16,6 +16,7 @@ import funciones as lc
 
 locale.setlocale(locale.LC_ALL, '')
 
+CABECERA = "Provincia;2017;2016;2015;2014;2013;2012;2011;2010"
 
 # Hay que usar diccionarios y numpy arrays. Prohibido
 # usar pandas y dataframe.
@@ -34,19 +35,20 @@ def salida_html_R1(fichero, html_crear):
     :return: None
     """
     # Leer el fichero CSV.
-    dict_r = lc.dict_fichero_csv(fichero)
+    dict_r = lc.dict_fichero_csv(fichero)       # Limpiamos el fichero csv.
 
-    p_poblacion = ""
-    fila_1 = dict_r.__next__()
-    datos_utiles = [columna for columna in fila_1.keys() if columna != 'none']
-    n_datos = len(datos_utiles)
-    n_years = n_datos - 2  # 2017 a 2010
+    # Diccionarios y estructuras que vamos a usar.
+    p_poblacion = ""            # String que vamos a escribir en el html.
+    fila_1 = dict_r.__next__()  # Saltamos la primera fila (datos que usamos para coger las cabeceras).
+    datos_utiles = [columna for columna in fila_1.keys() if columna != 'none']  # Cogemos los años que vamos a usar.
+    n_datos = len(datos_utiles)                 # Longitud de los datos.
+    n_years = n_datos - 2  # 2017 a 2011        # Número de años que vamos a usar menos la columna "Provincias".
 
     # Crear el fichero HTML.
     with open(html_crear, 'w', encoding='utf-8') as html:
         p_poblacion += lc.CABECERA_HTML
 
-        # Crear la tabla
+        # Crear la tabla con la estructura que queremos.
         tabla_inicio = """
         <table>
         <tr>
@@ -59,70 +61,55 @@ def salida_html_R1(fichero, html_crear):
         # html.write(tabla_inicio)
         p_poblacion += tabla_inicio
 
-        # Añadimos los años para la variación absoluta y relativa.
+        # Inicio de la estructura de la tabla.
         p_poblacion += "<tr>\n"
-        p_poblacion += "<th>%s</th>\n" % datos_utiles[0]
+        p_poblacion += "<th>%s</th>\n" % datos_utiles[0]    # Titulo de la primera columna.
+
+        # Doble for para colocar los años tanto en la variación absoluta como en la relativa.
         for i in range(1, n_years):
             p_poblacion += "<th>%s</th>\n" % datos_utiles[i]
+
         for i in range(1, n_years):
             p_poblacion += "<th>%s</th>\n" % datos_utiles[i]
+
         p_poblacion += "</tr>\n"
 
-        dict_r.__next__()  # Saltamos la primera fila (años).
+        dict_r.__next__()  # Saltamos la primera fila (Datos innecesarios).
         for a_dict in dict_r:
             # Añadimos las provincias.
             p_poblacion += "<tr>\n"
-            p_poblacion += "<td>%s</td>\n" % a_dict['Provincia']
+            p_poblacion += "<td>%s</td>\n" % a_dict['Provincia']        # Nombre de la provincia en la primera columna.
+
+            # Inicio celdas de datos.
             for i in range(1, n_years):
-                variacion_absoluta = (float(a_dict[datos_utiles[i]]) - float(a_dict[datos_utiles[i + 1]]))
+                variacion_absoluta = lc.variacion_absoluta(a_dict[datos_utiles[i]],
+                                                           a_dict[datos_utiles[i + 1]])
                 # Añadimos la variación absoluta.
-                p_poblacion += "<td>%s</td>\n" % locale.format_string('%.2f', variacion_absoluta, grouping=True)
+                p_poblacion += "<td>%s</td>\n" % variacion_absoluta
             for i in range(1, n_years):
-                variacion_relat = variacion_relativa(float(a_dict[datos_utiles[i]]),
+                variacion_relativa = lc.variacion_relativa(float(a_dict[datos_utiles[i]]),
                                                      float(a_dict[datos_utiles[i + 1]]))
                 # Añadimos la variación relativa.
-                p_poblacion += "<td>%s</td>\n" % locale.format_string('%.2f', variacion_relat, grouping=True)
+                p_poblacion += "<td>%s</td>\n" % variacion_relativa
+
+            # Fin celdas de datos.
             p_poblacion += "</tr>\n"
 
-        # html.write("</table>\n")
         p_poblacion += "</table>\n"
-        # html.write(lc.PIE_HTML)
         p_poblacion += lc.PIE_HTML
 
         html.write(p_poblacion)
-        fileEstilo = open("./salidas/estilo.css", "w", encoding="utf8")
-
-        estilo = """  table, th, td {
-                        border-collapse: collapse;    
-                        border:1px solid black;
-                        font-family: Arial, Helvetica, sans-serif;
-                        padding: 8px;
-                        text-align: center;
-                    }  """
-
-        fileEstilo.write(estilo)
-        fileEstilo.close()
-
-
-def variacion_relativa(anio_mayor, anio_menor):
-    """
-    Calcula la variación relativa entre dos años.
-    :param anio_mayor: Año mayor.
-    :param anio_menor: Año menor.
-    :return: Variación relativa entre ambos años.
-    """
-    return ((anio_mayor - anio_menor) / anio_menor) * 100
 
 
 def ejecutar_R1():
-    cabecera = "Provincia;2017;2016;2015;2014;2013;2012;2011;2010"
     lc.limpiar_csv('entradas/poblacionProvinciasHM2010-17.csv',
-                   "salidas/poblacionProvinciasHM2010-17.csv",
-                   cabecera, "Total", "Notas")
-    lc.leer_fichero('salidas/poblacionProvinciasHM2010-17.csv')
+                   "resultados/poblacionProvinciasHM2010-17.csv",
+                   CABECERA, "Total", "Notas")
 
-    salida_html_R1('salidas/poblacionProvinciasHM2010-17.csv',
-                   'salidas/salidaR1.html')
+    # lc.leer_fichero('resultados/poblacionProvinciasHM2010-17.csv')
+
+    salida_html_R1('entradas/poblacionProvinciasHM2010-17.csv',
+                   'resultados/variacionProvincias.html')
 
 
 # Programa principal.
