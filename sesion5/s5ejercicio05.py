ec62cccc316df4c92be1c6dc53a4dada8c736599
Ismael Díaz <xWaRzMiixZ@gmail.com>
2023-11-30 11:55:14 +0100

    SESION 5
    TODO: Ejercicios 5, 6, 7, 8

---
+++ sesion5/s5ejercicio05.py
@@ -0,0 +1,11 @@
+"""
+Desde python, crea un fichero de texto “capitales.csv” separado por “;” 
+donde figuren 4 nombres de paises seguidos de su capital. La primera línea 
+debe ser la cabecera: País;Capital, la segunda línea por ejemplo: España;Madrid 
+y así hasta 4 países. Usar csv.reader para leer el fichero y crear un diccionario1 
+donde las claves sean los paises y los valores sean cada capital. Usar luego 
+csv.DictReader para generar un segundo diccionario2 con la misma información.
+"""
+
+# -*- coding: utf-8 -*-
+
